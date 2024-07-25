b2cc0b7943025d8a71d8fdf015862b47fb932485
ShumsAn <shum-sol@outlook.com>
2024-07-25 12:12:34 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,14 +1,13 @@
 import random
 
 n = random.choice(range(3,21))
-
 result =''
+
 for i in range(1,n):
     for j in range(i+1, n):
         if n % (i + j) == 0:
-
             result += f'{i}{j}'
-            #result = str(i)+str(j)
+            
 
 print(f'{n} - {result}')
 
